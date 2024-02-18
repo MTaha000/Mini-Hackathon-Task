15c7d429e502f64eecbddd214af150e4a96c7725
Taha <itstaha82@gmail.com>
2024-02-18 19:02:56 +0500

    Animation Changes

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
-let display0 = document.querySelector(".d0")
 let display1 = document.querySelector(".d1")
 let display2 = document.querySelector(".d2")
 let display3 = document.querySelector(".d3")
 
-let active = false;
-let active2 = false;
-let active3 = false;
+let title = "gh repo view"
 
-setInterval(() => {
-    if (!active) {
-        display1.classList.add("show")
-        display0.classList.add("visible")
-        active = true;
+let index = 1;
+
+
+const typeWriterAnimation = () => {
+    let new_title = title.slice(0, index);
+    display1.innerHTML = new_title;
+    display2.innerHTML = new_title;
+    index > title.length ? index = 1 : index++;
+    if (index > title.length - 1) {
+        display3.innerHTML = ` <p>cli/cli</p>
+    <p>GitHub’s official command line tool</p>
+    <div class="main-content">
+        <span>GitHub CLI</span>
+        <div class="first-content">
+            <p><span>gh</span>is GitHub on the command line. It brings pull requests, issues,
+                and
+                other GitHub concepts to the terminal next to where you are already working with
+                <span>git</span> and your code.
+            </p>
+        </div>
+        <div class="second-content">
+            <p>Image: screenshot of gh pr status →
+                <span>https://user-images.githubusercontent.com/98482/84171218-327e7a80-aa40-11ea-8cd1-5177fc2d0e72.png</span>
+            </p>
+        </div>
+    </div>
+    <p style="color: var(--secondary-text);">View this repository on GitHub:
+        https://github.com/cli/cli</p>`
+    } else {
+        display3.innerHTML = "";
     }
-}, 2000)
 
+    if (index > title.length) {
+        setTimeout(() => typeWriterAnimation(), 3000)
 
-
-setInterval(() => {
-    if (!active2) {
-        display2.classList.add("show")
-        active2 = true
+    } else {
+        setTimeout(() => typeWriterAnimation(), 300)
     }
-}, 2300)
+}
 
-
-
-setInterval(() => {
-    if (!active3) {
-        display3.classList.add("show")
-        active3 = true;
-    }
-
-}, 2500)
-
-setInterval(() => {
-    if (active) {
-        display1.classList.remove("show")
-        display0.classList.remove("visible")
-        active = false;
-    }
-    if (active2) {
-        display2.classList.remove("show")
-        active2 = false;
-    }
-    if (active3) {
-        display3.classList.remove("show")
-        active3 = false;
-    }
-}, 5000)
+typeWriterAnimation();
 
